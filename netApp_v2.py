97e7ca1b5fd6ca34b0f254b200b3b5ac09fb7e87
Mariaaanne <33870061+Mariaaanne@users.noreply.github.com>
2017-11-28 15:08:54 +0100

    z

--- netApp_v2.py
+++ netApp_v2.py
@@ -60,7 +60,7 @@ while True:
 		moyenne = mean(liste_valeurs)
 		
 		if moyenne > seuil:
-			timeStamp = time.time()
+			timeStamp = str(time.time())
 			print("LET n{}; {}".format(indice_let, timeStamp))
 			indice_let = indice_let + 1
 			diode = subprocess.Popen("python ./ledApp_5s.py", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
