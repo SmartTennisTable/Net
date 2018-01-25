eba8dace7ec6309657fbf3b67204ea9d5967b770
Boidard <damien.boidard@bearingpoint.com>
2018-01-25 11:07:25 +0100

    test mqtt

---
+++ broker.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(("/test", 1))
+    client.subscribe(("iot/data/iotmmsp1942978066trial/v1/85732072-22b7-4cd1-ae8f-d363975c0f91", 1))
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set("capllkmg", password="TYk-UHLw95TH")
+
+client.connect("m14.cloudmqtt.com", 15839, 60)
+
+client.loop_forever()
